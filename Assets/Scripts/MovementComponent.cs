48f8737fbeb90d7f8ab3f9d31b45496339048294
HermanHum <HermanHmHum@gmail.com>
2022-01-26 15:00:24 -0500

    Gun asset and firing & reloading animations

--- Assets/Scripts/MovementComponent.cs
+++ Assets/Scripts/MovementComponent.cs
@@ -12,53 +12,67 @@ public class MovementComponent : MonoBehaviour
     private float runSpeed = 10;
     [SerializeField]
     private float jumpForce = 5;
+    [SerializeField]
+    private Transform followTransform;
+    [SerializeField]
+    private float aimSensitivity = 1f;
+    private Vector2 rotationVector;
 
     // Components
     private PlayerController playerController;
-    private Rigidbody rigidbody;
+    private Rigidbody rigidBody;
     private Animator playerAnimator;
 
     // Movement references
     Vector2 inputVector = Vector2.zero;
     Vector3 moveDirection = Vector3.zero;
+    Vector2 lookInput = Vector2.zero;
 
     public readonly int movementXHash = Animator.StringToHash("MovementX");
     public readonly int movementYHash = Animator.StringToHash("MovementY");
     public readonly int isRunningHash = Animator.StringToHash("isRunning");
     public readonly int isJumpingHash = Animator.StringToHash("isJumping");
+    public readonly int isFiringHash = Animator.StringToHash("isFiring");
+    public readonly int isReloadingingHash = Animator.StringToHash("isReloading");
 
     private void Awake()
     {
         playerController = GetComponent<PlayerController>();
-        rigidbody = GetComponent<Rigidbody>();
+        rigidBody = GetComponent<Rigidbody>();
         playerAnimator = GetComponent<Animator>();
-    }
-
-    // Start is called before the first frame update
-    private void Start()
-    {
-        
     }
 
     // Update is called once per frame
     private void Update()
     {
-        //if (playerController.isJumping) return;
-        if (!(inputVector.magnitude > 0)) moveDirection = Vector3.zero;
+        //camera
+        rotationVector += new Vector2(-lookInput.y, lookInput.x) * aimSensitivity;
+        rotationVector.x = Mathf.Clamp(rotationVector.x, -80.0f, 80.0f);
 
+        transform.rotation = Quaternion.Euler(0f, rotationVector.y, 0.0f);
+        followTransform.localRotation = Quaternion.Euler(rotationVector.x, 0f, 0f);
+
+        // Movement
         moveDirection = transform.forward * inputVector.y + transform.right * inputVector.x;
         float currentSpeed = playerController.isRunning ? runSpeed : walkSpeed;
 
-        Vector3 movementDirection = moveDirection * currentSpeed * Time.deltaTime;
+        Vector3 moveVelocity = moveDirection * currentSpeed;
+        if (playerController.isJumping)
+        {
+            rigidBody.AddForce(moveVelocity * Time.deltaTime, ForceMode.VelocityChange);
+        }
+        else
+        {
+            rigidBody.velocity = moveVelocity + rigidBody.velocity.y * Vector3.up;
+        }
 
-        transform.position += movementDirection;
+        playerAnimator.SetFloat(movementXHash, inputVector.x, 1, 0.1f);
+        playerAnimator.SetFloat(movementYHash, inputVector.y, 1, 0.1f);
     }
 
     public void OnMovement(InputValue value)
     {
         inputVector = value.Get<Vector2>();
-        playerAnimator.SetFloat(movementXHash, inputVector.x);
-        playerAnimator.SetFloat(movementYHash, inputVector.y);
     }
 
     public void OnRun(InputValue value)
@@ -72,8 +86,31 @@ public class MovementComponent : MonoBehaviour
         if (playerController.isJumping) return;
 
         playerController.isJumping = true;
-        rigidbody.AddForce(transform.up * jumpForce + moveDirection * walkSpeed, ForceMode.Impulse);
+        rigidBody.AddForce(transform.up * jumpForce + moveDirection * walkSpeed * Time.deltaTime, ForceMode.Impulse);
         playerAnimator.SetBool(isJumpingHash, true);
+    }
+
+    public void OnLook(InputValue value)
+    {
+        lookInput = value.Get<Vector2>();
+        // If we aim up, down, adjust animations to have a mask that will let us properly animate aim
+    }
+
+    public void OnReload(InputValue value)
+    {
+        playerController.isReloading = value.isPressed;
+        playerAnimator.SetTrigger(isReloadingingHash);
+    }
+
+    public void OnAim(InputValue value)
+    {
+        playerController.isAiming = value.isPressed;
+    }
+
+    public void OnFire(InputValue value)
+    {
+        playerController.isFiring = value.isPressed;
+        playerAnimator.SetBool(isFiringHash, value.isPressed);
     }
 
     private void OnCollisionEnter(Collision collision)
